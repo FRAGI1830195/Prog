2ba5059afe13820fbaab23211c546b25ca1b2296
FRAGI1830195 <54862920+FRAGI1830195@users.noreply.github.com>
2019-11-23 00:35:34 -0500

    Défi - Krypton

---
+++ Atelier/Décodage Krypton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krypton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string messageTxt = "Mb'qsph'd'ftu'm(gvo";
+            string NouveauMessage = "";
+
+            for (int i = 0; i<messageTxt.Length; i++)
+            {
+                int valeurAsc = (int)messageTxt[i];
+                NouveauMessage += Char.ConvertFromUtf32(valeurAsc - 1);
+            }
+            Console.WriteLine(NouveauMessage);
+            Console.ReadLine();        }
+    }
+}
---
+++ Atelier/Encodage Krypton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DéKrypton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "La_kryptonite_est_un_danger";
+            string nouveauMessage = "";
+            for (int i = 0; i < message.Length; i++)
+            {
+                int valeurAsc = (int)message[i];
+                nouveauMessage += Char.ConvertFromUtf32(valeurAsc + 1);
+            }
+            Console.WriteLine(nouveauMessage);
+        }
+    }
+}
